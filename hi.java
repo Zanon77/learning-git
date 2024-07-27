586f1461825af9c101c18f928d050631786433a9
Chaman <chamansinha9480@gmail.com>
2024-07-27 19:49:34 +0530

    Newly updated java classes

--- hi.java
+++ hi.java
@@ -1,6 +1,6 @@
 public class hi{
     public static void main(String [] args){
-        System.out.println("HII Class Called");
+        System.out.println("HII Cl222ass Called");
         System.out.println("C111haman is Class Called 222");
     }
 }
