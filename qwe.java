d65b7b480443f566ad8be540a656cddbdfb1806c
Chaman <chamansinha9480@gmail.com>
2024-07-27 20:47:38 +0530

    Nice

---
+++ qwe.java
@@ -0,0 +1,6 @@
+public class qwe{
+    public static void main(String [] args){
+        System.out.println("HII Cl222ass Called");
+        System.out.println("C111haman is Class Called 222");
+    }
+}
