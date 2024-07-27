21c0c99c5f825fd1f9553f1dff4418c8c604a610
Chaman <chamansinha9480@gmail.com>
2024-07-27 19:48:04 +0530

    Updating classes

---
+++ hey.java
@@ -0,0 +1,8 @@
+public class hey{
+    public static void main(String [] args){
+        int a=5;
+        int b=7;
+        int c=a+b;
+        System.out.println(c+" Hello Class Called "+a+b);
+    }
+}
