fa4f450facf1d4959594f31f8a24a4a0771e60fd
Chaman <chamansinha9480@gmail.com>
2024-09-10 02:57:41 +0530

    Master calls

--- hi.java
+++ hi.java
@@ -1,6 +1,6 @@
 public class hi{
     public static void main(String [] args){
         System.out.println("Hi Class Called");
-        System.out.println("Chaman is Called");
+        System.out.println("Master made the Call");
     }
 }
