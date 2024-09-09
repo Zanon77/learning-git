715f245b5d244f7551690a4d791457843ac09b99
Chaman <chamansinha9480@gmail.com>
2024-09-10 02:49:13 +0530

    Hello Updated

--- hello.java
+++ hello.java
@@ -1,6 +1,9 @@
 public class hello{
     public static void main(String [] args){
+        
+        System.out.println("John is my name..");
         System.out.println("Hello Class Called");
+        
         
     }
 }
